17c45e437ecf15256e465e8e90c672a57b26e33a
Charles Serrato <serrato.charles@gmail.com>
2024-04-23 23:24:24 +1200

    Entity movement + Camera follow + Path interpolation
    Entity now moves along the pre-set path. Camera now chases behind the entity when playing. Path interpolation changed to make paths curved instead of having sharp turns.

--- bitw_script.js
+++ bitw_script.js
@@ -4,26 +4,28 @@ Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOi
 // Initialise viewer
 const viewer = new Cesium.Viewer("cesiumContainer", {
   terrain: Cesium.Terrain.fromWorldTerrain(),
+  infoBox: false,
+  selectionIndicator: false,
 });
-
-// Visualise buildings
-const buildingTileSet = await Cesium.createOsmBuildingsAsync();
-viewer.scene.primitives.add(buildingTileSet);
 
 // The viewer object's camera
 const cam = viewer.camera;
 
 // Set default camera pitch
 const pitchAngle = Cesium.Math.toRadians(-15.0);
-const cameraOffset = new Cesium.HeadingPitchRange(0.0, pitchAngle, 150.0);
+const cameraOffset = new Cesium.HeadingPitchRange(0.0, pitchAngle, 300.0);
+
+// Visualise buildings
+const buildingTileSet = await Cesium.createOsmBuildingsAsync();
+viewer.scene.primitives.add(buildingTileSet);
 
 // UoW Student Centre Coordinates
 const uowsc = Cesium.Cartesian3.fromDegrees(175.3177, -37.78765, 100.0);
 // SAMPLE DATA. Change using wind data
 const pos1 = Cesium.Cartesian3.fromDegrees(175.3197, -37.78765, 100.0);
-const pos2 = Cesium.Cartesian3.fromDegrees(175.3137, -37.78635, 150.0);
-const pos3 = Cesium.Cartesian3.fromDegrees(175.3157, -37.78455, 125.0);
-const pos4 = Cesium.Cartesian3.fromDegrees(175.3167, -37.78625, 110.0);
+const pos2 = Cesium.Cartesian3.fromDegrees(175.3157, -37.78455, 125.0);
+const pos3 = Cesium.Cartesian3.fromDegrees(175.3137, -37.78635, 150.0);
+const pos4 = Cesium.Cartesian3.fromDegrees(175.3123, -37.78675, 110.0);
 const pos5 = Cesium.Cartesian3.fromDegrees(175.3127, -37.78885, 100.0);
 const pos6 = uowsc;
 const pos_array = [pos1, pos2, pos3, pos4, pos5, pos6];
@@ -66,8 +68,8 @@ function createPath() {
     nextTimeStep = time;
     
     // Format date and time for easier reading
-    const newDate = Cesium.JulianDate.toGregorianDate(time, new Cesium.GregorianDate())
-    const text = newDate.year + "/" + newDate.month + "/" + newDate.day + " | "
+    const newDate = Cesium.JulianDate.toGregorianDate(time, new Cesium.GregorianDate());
+    const text = "pos" + (i+1) + " -- " + newDate.year + "/" + newDate.month + "/" + newDate.day + " | "
                + newDate.hour + ":" + newDate.minute + ":" + newDate.second;
 
     // Create entity based on sample position
@@ -85,7 +87,7 @@ const flightPath = createPath();
 
 // Create entity
 const targetEntity = viewer.entities.add({
-  name: "Target entity to track",
+  name: "The hot air balloon",
   // Move entity via simulation time
   availability: new Cesium.TimeIntervalCollection([
     new Cesium.TimeInterval({
@@ -95,9 +97,11 @@ const targetEntity = viewer.entities.add({
   ]),
   // Use path created by the function
   position: flightPath, 
+  // Automatically compute orientation based on position movement.
+  orientation: new Cesium.VelocityOrientationProperty(flightPath),
   // Placeholder entity visuals
   ellipsoid: {
-    radii: new Cesium.Cartesian3(15.0, 15.0, 15.0),
+    radii: new Cesium.Cartesian3(12.0, 12.0, 12.0),
     material: Cesium.Color.RED.withAlpha(0.5),
     outline: true,
     outlineColor: Cesium.Color.BLACK,
@@ -113,15 +117,58 @@ const targetEntity = viewer.entities.add({
   },
 });
 
-// Set viewer's tracked entity
-viewer.trackedEntity = targetEntity;
-
-// Fly to entity (Viewer)
-const result = viewer.flyTo(targetEntity, {
-  offset: cameraOffset,
+// Change interpolation mode to make path more curved
+targetEntity.position.setInterpolationOptions({
+  interpolationDegree: 5,
+  interpolationAlgorithm:
+    Cesium.LagrangePolynomialApproximation,
 });
 
-// Async promise. result will update after flyTo completes (or otherwise)
-if (result) {
-  console.log("flyTo complete...");
+//Set up chase camera
+var matrix3Scratch = new Cesium.Matrix3();
+var positionScratch = new Cesium.Cartesian3();
+var orientationScratch = new Cesium.Quaternion();
+var scratch = new Cesium.Matrix4();
+
+function getModelMatrix(targetEntity, time, result) {
+  var position = Cesium.Property.getValueOrUndefined(targetEntity.position, time, positionScratch);
+  if (!Cesium.defined(position)) {
+    return undefined;
+  }
+  var orientation = Cesium.Property.getValueOrUndefined(targetEntity.orientation, time, orientationScratch);
+  if (!Cesium.defined(orientation)) {
+    result = Cesium.Transforms.eastNorthUpToFixedFrame(position, undefined, result);
+  } else {
+    result = Cesium.Matrix4.fromRotationTranslation(Cesium.Matrix3.fromQuaternion(orientation, matrix3Scratch), position, result);
+  }
+  return result;
 }
+
+// Fly to entity (Viewer)
+// viewer.flyTo(targetEntity, {
+//   offset: cameraOffset,
+// });
+
+// Quick camera focus to target entity
+viewer.zoomTo(targetEntity, cameraOffset);
+
+/* *****RUNTIME CODE***** */
+// Tick function
+viewer.clock.onTick.addEventListener(function(clock) {
+  // If clock is playing
+  if(clock.shouldAnimate) {
+    // Change camera angle to 3rd person view (chase cam)
+    getModelMatrix(targetEntity, viewer.clock.currentTime, scratch);
+    cam.lookAtTransform(scratch, new Cesium.Cartesian3(-250, 0, 70));
+  }
+});
+
+// On Pause/Play event
+Cesium.knockout.getObservable(viewer.animation.viewModel.clockViewModel, 
+                              'shouldAnimate').subscribe(function(value) {
+  // If paused
+  if (!value) {
+    // Revert camera back to normal
+    viewer.zoomTo(targetEntity, cameraOffset);
+  }
+});
