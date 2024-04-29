fcbf259662d65d36ddc4ffe08affc8055458f44e
Musawar-A <musawarahmad05@gmail.com>
2024-04-29 13:34:46 +1200

    Balloon overlay on cesium

---
+++ README.md
@@ -0,0 +1,2 @@
+# BlowingInTheWind
+COMPX241 Smoke and Mirrors Project - A hot air balloon simulator using real world weather and mapping data
---
+++ balloon.js
@@ -0,0 +1,70 @@
+//Import the THREE.js library
+import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
+// To allow for importing the .gltf file
+import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
+
+//Create a Three.JS Scene
+const scene = new THREE.Scene();
+//create a new camera with positions and angles
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+//Keep the 3D object on a global variable so we can access it later
+let object;
+
+//Instantiate a loader for the .gltf file
+const loader = new GLTFLoader();
+
+//Load the file
+loader.load(
+  '/Balloon/scene.gltf',
+  function (gltf) {
+    //If the file is loaded, add it to the scene
+    object = gltf.scene;
+    scene.add(object);
+    
+    object.position.set( 0, -9 ,  0);
+    object.rotation.x = 0.55;
+    //object.rotation.y = 0.4;
+    
+  },
+  function (xhr) {
+    //While it is loading, log the progress
+    console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+  },
+  function (error) {
+    //If there is an error, log it
+    console.error(error);
+  }
+);
+
+//Instantiate a new renderer and set its size
+const renderer = new THREE.WebGLRenderer({ alpha: true }); //Alpha: true allows for the transparent background
+renderer.setSize(window.innerWidth/2, window.innerHeight/2);
+
+//Add the renderer to the DOM
+document.getElementById("balloonContainer").appendChild(renderer.domElement);
+
+//Set how far the camera will be from the 3D model
+camera.position.z = 25;
+
+//Add lights to the scene, so we can actually see the 3D model
+const topLight = new THREE.DirectionalLight(0xffffff, 1); // (color, intensity)
+topLight.position.set(500, 500, 500) //top-left-ish
+topLight.castShadow = true;
+scene.add(topLight);
+
+const ambientLight = new THREE.AmbientLight(0x333333);
+scene.add(ambientLight);
+
+//Render the scene
+function animate() {
+    requestAnimationFrame(animate);
+  //Here we could add some code to update the scene, adding some automatic movement
+    if(object) object.rotation.y += 0.005;
+    renderer.render(scene, camera);
+}
+
+
+
+//Start the 3D rendering
+animate();
